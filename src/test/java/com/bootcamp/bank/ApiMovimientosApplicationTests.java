ca3d255e4fcf3d70991c9f1585746f74ee7a9475
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-17 17:18:37 -0500

    sonar fix

--- src/test/java/com/bootcamp/bank/ApiMovimientosApplicationTests.java
+++ src/test/java/com/bootcamp/bank/ApiMovimientosApplicationTests.java
@@ -1,13 +1,19 @@
 package com.bootcamp.bank;
 
+import com.bootcamp.bank.movimientos.service.MovimientoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ApiMovimientosApplicationTests {
-
+	@Autowired
+	private MovimientoService movimientoService;
 	@Test
 	void contextLoads() {
+		assertThat(movimientoService).isNotNull();
 	}
 
 }
