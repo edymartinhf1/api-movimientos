09981f8c4ad3de8d93696ef281f457354048a8b2
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-17 19:48:58 -0500

    sonar fix

--- src/main/java/com/bootcamp/bank/movimientos/exception/BusinessException.java
+++ src/main/java/com/bootcamp/bank/movimientos/exception/BusinessException.java
@@ -1,12 +1,9 @@
 package com.bootcamp.bank.movimientos.exception;
 
 public class BusinessException extends RuntimeException{
-    private String errorCode;
-    private String messageError;
 
     public BusinessException(String messageError) {
         super(messageError);
-        this.messageError = messageError;
     }
 
 
